bf75fdb3422da3520c71b69776e6dd7d015d8059
zhanchi5 <zhanchi5@outlook.com>
2017-03-15 10:39:45 +0300

    Update overload_functions.cpp

--- overload_functions.cpp
+++ overload_functions.cpp
@@ -2,58 +2,63 @@
 #include <vector>
 #include <algorithm>
 #include <ctime>
-#include <math.h>
+#include <cmath>
 
-// Проверим, есть ли изменения при добавлении кода
+
+#define PI 3.14159265
+
+
+
 using namespace std;
+
 
 class Areas
 {
-
-float a,b,c,S;   // длины сторон
-int q;    // угол в градусах
 public:
+    double Triangle(float a,float b, float q)
+    {
+        0.5*a*b*sin(q * PI/180);
+        return 0.5 * a * b * sin(q * PI/180);
 
-float Triangle(a,b,q)
-{
-        S = 1/2 * a * b*sin(q);
-        return S;
+    }
 
-}
-void Triangle(a,b,c)
-{
-        p = 1/2*(a+b+c);
-        S = sqrt(p*(p-a)*(p-b)*(p-c));
-        return S;
+    double Triangle(float a, float b,float c, float p)
+    {
+        p = (0.5*(a+b+c));
+        sqrt(p*(p-a)*(p-b)*(p-c));
+        return sqrt(p*(p-a)*(p-b)*(p-c));
 
-}
+    }
 
-}
+};
 
-Triangle(float a, float b,float c);
-Triangle(float a, float b, int q);
 
 int main()
 {
-        float a,b,c,S;
-        int q;
+   
+    float a = 0,b = 0,c = 0, p;
+    int q = 0;
+    Areas S1,S2;
 
-        cout << "Введите длины стороны: "<< endl;
-        cout << "a= ";
-        cin >> a;
-        cout << "b= ";
-        cin >> b;
-        cout << "c= ";
-        cin >> c;
+    cout << "Введите длины стороны: "<< endl;
+    cout << "a= ";
+    cin >> a;
+    cout << "b= ";
+    cin >> b;
+    cout << "c= ";
+    cin >> c;
 
-        cout << "Введите угол между сторонами a и b в градусах" << endl;
-        cout << "q= ";
-        cin >> q;
-        cout << "S1= " << Triangle(a,b,c) << endl;
-        cout << "S2= " << Triangle(a,b,q) << endl;
+    S1.Triangle(a,b,c,p);
+    S2.Triangle(a,b,q);
+
+    cout << "Введите угол между сторонами a и b в градусах" << endl;
+    cout << "q= ";
+    cin >> q;
+    cout << "S1= " << S1.Triangle(a,b,c,p) << endl;
+    cout << "S2= " << S2.Triangle(a,b,q) << endl;
 
 
 
-        cin.get();
-        return 0;
+    cin.get();
+    return 0;
 }
